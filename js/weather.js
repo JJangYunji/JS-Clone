e63912bd6fa80bfabe1ca67db9fe9a2f48bb8952
JJangYunji <80668447+JJangYunji@users.noreply.github.com>
2021-11-15 01:43:46 +0900

    Create weather.js

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+const API_KEY = "f0596242eae30a6fbfbeb10b0666611b";
+
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+
+      weather.innerText = `${data.main.temp}°, ${data.weather[0].main}`;
+      city.innerText = data.name;
+    });
+}
+
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
